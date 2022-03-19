269aa3163b86c06b21a3bbb9ab0784a872a69e47
maxmaster <maxmaster82@gmail.com>
2022-03-19 11:15:25 +0200

    Setup general deploy flow

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ const port = PORT;
 
 app.get('/', (req, res) => {
 	console.log(PORT);
-	res.send('Docker vet demo!!!!');
+	res.send('Docker vet app demo 1.0.1 !!!!');
 });
 
 app.listen(port, () => {
